cea95fe9c7ec68b4d55066db44c9573b28e4a251
Luigi-Teodoro <luigi.tc14@gmail.com>
2025-01-25 19:10:26 -0500

    actualizacion

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,19 @@ router.get('/register', (req, res) => res.render('regis', {title:'Registro'}));
 router.get('/informacion', (req, res) => res.render('sobre_nosotros', {title:'Nosotros'}));
 router.get('/infochaqueta', (req, res) => res.render('info_chaqueta', {title:'Chaqueta'}));
 router.get('/infoshort', (req, res) => res.render('info_short', {title:'Short'}));
+router.get('/infopolo', (req, res) => res.render('info_polo', {title:'Polo'}));
+router.get('/infopolonike', (req, res) => res.render('info_polo_nike', {title:'Polo Nike'}));
+router.get('/infochaquetabomber', (req, res) => res.render('info_chaqueta_bomber', {title:'Chaqueta Bomber'}));
+router.get('/infoshortalgodon', (req, res) => res.render('info_short_algodon', {title:'Short Algodon'}));
+router.get('/infojean', (req, res) => res.render('info_jean', {title:'Jean'}));
+router.get('/infojean2', (req, res) => res.render('info_jean2', {title:' Atheletic Jean'}));
+router.get('/solochaquetas', (req, res) => res.render('solo_chaquetas', {title:'Chaquetas'}));
+router.get('/soloshort', (req, res) => res.render('solo_short', {title:'Shorts'}));
+router.get('/solopolos', (req, res) => res.render('solo_polos', {title:'Polos'}));
+router.get('/solojeans', (req, res) => res.render('solo_jeans', {title:'Jeans'}));
+
+
+
 
 
 export default router;
